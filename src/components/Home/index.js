fdadd977e02f17d1ad78cdc45ff917f83a4e94b7
CodeWithYash99 <yashwanthrangu99@gmail.com>
2023-10-09 19:15:16 +0530

    Add Contact Section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import Navbar from '../Navbar'
 import About from '../About'
 import Skills from '../Skills'
 import Projects from '../Projects'
+import Contact from '../Contact'
 
 import './index.css'
 
@@ -23,6 +24,7 @@ const Home = () => {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   )
 }
